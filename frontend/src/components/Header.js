32220757427de6b94c362329843cd2f5cc89871e
Ceana Daniel <careendaniel1996@gmail.com>
2022-03-12 13:28:24 -0600

    favicon,Header and Footer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
 const Header = () => {
   return (
-    <Header>
-        Header
-    </Header>
+    <header>
+      <Navbar bg="black" variant="blue" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">MyTket</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart"><i className="fa-solid fa-user"></i> </Nav.Link>
+              <Nav.Link href="/login"> Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   )
 }
 
