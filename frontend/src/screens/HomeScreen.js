22f2072f80b2a60533c232b0e0a7fa1e4146ee9e
Ceana Daniel <careendaniel1996@gmail.com>
2022-03-28 01:15:48 -0500

    frontend to backend events

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Row,Col} from 'react-bootstrap'
 import Event from '../components/Event'
-import events from '../events'
+import axios from 'axios'
+
+
 
 const HomeScreen = () => {
+  const [events, setEvents] = useState([])
+
+  useEffect(()=>{
+  const fetchEvents = async() => {
+    const {data} = await axios.get('/api/events')
+   setEvents(data) 
+  }
+  fetchEvents()
+  }, [])
+
   return (
     <>
         <h1>Latest Events</h1>
