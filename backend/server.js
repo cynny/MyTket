cd0fe0b573796b06d08e7efd480443c7731ee2fa
cynny <99002017+cynny@users.noreply.github.com>
2022-04-14 01:28:57 -0500

    created eventroutes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import events from './data/events.js'
 import connectDB from './config/db.js'
+import eventRoutes from './routes/eventRoutes.js'
 
 const app = express()
 dotenv.config()
 connectDB()
 
-app.get('/api/events', (req, res) => {
-    res.json(events)
-})
-
-app.get('/api/events/:id', (req, res) => {
-  const event = events.find((e)  => e._id === req.params.id);
-  res.json(event);
-})
+app.use('/api/events', eventRoutes)
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
