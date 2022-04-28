38bf5f297f63e9303d1650802eb20d45a6470161
khakas <kavithahakas@gmail.com>
2022-04-27 18:47:17 -0700

    Backend fixes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import eventRoutes from './routes/eventRoutes.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 
 const app = express()
@@ -10,8 +10,8 @@ dotenv.config()
 connectDB()
 
 app.use('/api/events', eventRoutes)
-app.use(errorHandler)
 app.use(notFound)
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
---
+++ frontend/src/reducers/eventReducers.js
@@ -0,0 +1,17 @@
+import { EVENT_LIST_FAIL, EVENT_LIST_REQUEST, EVENT_LIST_SUCCESS } from "../constants/eventsConstants";
+
+
+export const eventListReducer = (state = {events: []},
+     action) => {
+         switch (action.type) {
+             case EVENT_LIST_REQUEST:
+                 return {loading: true, events: []}
+             case EVENT_LIST_SUCCESS:
+                 return {loading: false, events: action.payload}
+             case EVENT_LIST_FAIL:
+                 return {loading: false, error: action.payload}
+
+             default:
+                 return state
+         }
+     }
