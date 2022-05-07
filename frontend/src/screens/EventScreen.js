fc4eeb1c53cfc498ae12ac921d421000c92c9684
cynny <99002017+cynny@users.noreply.github.com>
2022-05-07 02:57:40 -0500

    Update EventScreen.js

--- frontend/src/screens/EventScreen.js
+++ frontend/src/screens/EventScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button, Form} from 'react-bootstrap'
 import axios from 'axios'
 import Rating from '../components/Rating'
@@ -12,6 +12,7 @@ const EventScreen = () => {
   const [qty, setQty] = useState(0);
   const params = useParams ();
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const eventDetails = useSelector((state) => state.
   eventDetails)
   const { loading, error, event } = eventDetails
@@ -20,6 +21,9 @@ const EventScreen = () => {
    dispatch(listEventDetails(params.id))
   }, [dispatch, params])
 
+  const addToCartHandler = () => {
+    navigate(`/cart/${params.id}?qty=${qty}`)
+  }
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
@@ -69,8 +73,12 @@ const EventScreen = () => {
                     <Form.Control
                     as='select'
                     value={qty}
+                    onChange={(e) => setQty(e.target.value)}
                     >
-
+                      {[...Array(event.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>{x + 1}</option>
+                      ))
+                      }
                       </Form.Control></Col>
                   </Row>
                 </ListGroup.Item>
@@ -80,6 +88,7 @@ const EventScreen = () => {
                 <Button
                   className='btn-block'
                   type='button'
+                  onClick={addToCartHandler}
                   disabled={event.countInStock === 0}
                 >
                   Add To Cart
